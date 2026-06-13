280309a6313cec4e80f00605c82d43f8a34337bb
Sworup Ghimire <noriblademe1@gmail.com>
2026-06-13 10:29:50 +0545

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  root: 'code',
-  
   build: {
-    outDir: '../dist',
+    outDir: 'dist',
     emptyOutDir: true,
   }
 });
